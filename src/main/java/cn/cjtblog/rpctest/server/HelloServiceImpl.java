2c5e0eb32b6207db0130eae4943475b52bff0949
jintaocai <jintaocai@qq.com>
2016-03-07 17:40:08 +0800

    加入测试代码

---
+++ src/main/java/cn/cjtblog/rpctest/server/HelloServiceImpl.java
@@ -0,0 +1,19 @@
+package cn.cjtblog.rpctest.server;
+
+import com.xxx.rpc.sample.api.HelloService;
+import com.xxx.rpc.sample.api.Person;
+import com.xxx.rpc.server.RpcService;
+
+@RpcService(HelloService.class)
+public class HelloServiceImpl implements HelloService {
+
+    @Override
+    public String hello(String name) {
+        return "Hello! " + name;
+    }
+
+    @Override
+    public String hello(Person person) {
+        return "Hello! " + person.getFirstName() + " " + person.getLastName();
+    }
+}
